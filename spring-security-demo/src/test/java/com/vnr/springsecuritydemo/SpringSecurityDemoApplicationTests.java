3f3bb37671cc66e67bcf4d7994278d52a57fc450
manuel <nguyenmanhhuan.cfos@gmail.com>
2025-12-28 12:52:24 +0100

    first commit

---
+++ spring-security-demo/src/test/java/com/vnr/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnr.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
